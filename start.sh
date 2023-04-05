596ebfcb8b79468faae57c6963587731d148d61c
bsefwe <92920899+bsefwe@users.noreply.github.com>
2023-04-05 04:47:45 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # set val
 python3 /app/self-ping.py &
-
+PORT=8080
 AUUID=5aaed9b7-7fe3-47c3-bb52-db59859ce198
 ParameterSSENCYPT=chacha20-ietf-poly1305
 CADDYIndexPage=https://raw.githubusercontent.com/caddyserver/dist/master/welcome/index.html
